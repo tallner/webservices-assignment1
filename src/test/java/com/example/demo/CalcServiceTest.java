ce3f49629ce05a9945a243cb1c26c0a7b3ad458b
tallner <ctallner@hotmail.com>
2022-03-20 15:50:17 +0100

    Update CalcServiceTest.java

--- src/test/java/com/example/demo/CalcServiceTest.java
+++ src/test/java/com/example/demo/CalcServiceTest.java
@@ -10,35 +10,134 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class CalcServiceTest {
 	
-	private CalcService sut1;
-	//private CalcController sut2;
+	private CalcService sut;
 
-	@Before //körs innan test
+	@Before
 	public void setUp() {
-		sut1 = new CalcService(); //klassen som testas initieras innan test börjar
-		//sut2 = new CalcController();
+		sut = new CalcService();
 	}
-
+	
+	
+	
+	//test for correct result
 	@Test
 	public void testSubSucc() {
-		String actual = sut1.Substraction("5", "4");
+		String actual = sut.Substraction("5", "4");
 		String expected = "Result: 1";
 		assertEquals(expected, actual);
-		
-		//String actual2 =  sut2.substraction("7", "6");
-		//assertEquals(expected, actual2);
-
-		//assertTrue(actual == expected);
-
+	}
+	
+	//test for wrong result
+	@Test
+	public void testSubFail() {
+		String actual = sut.Substraction("5","6");
+		String expected = "56";
+		assertNotEquals(expected, actual);
+	}
+	
+	//test for wrong input format
+	@Test(expected = NumberFormatException.class)
+	public void testSub_badInput() {
+		String actual = sut.Substraction("a", "4");
+	}
+	
+	
+	
+	
+	
+	@Test
+	public void testAddSucc() {
+		String actual = sut.Addition("5", "4");
+		String expected = "Result: 9";
+		assertEquals(expected, actual);
 	}
 	
 	@Test
-	public void testSubFail() {
-		String actual = sut1.Substraction("5","6");
+	public void testAddFail() {
+		String actual = sut.Addition("5","6");
 		String expected = "56";
-		//assertTrue(actual == expected);
-		
+		assertNotEquals(expected, actual);
+	}
+	
+	//test for wrong input format
+	@Test(expected = NumberFormatException.class)
+	public void testAdd_badInput() {
+		String actual = sut.Addition("a", "4");
+	}
+	
+	
+	
+	
+	
+	@Test
+	public void testMultSucc() {
+		String actual = sut.Multiplication("5", "4");
+		String expected = "Result: 20";
 		assertEquals(expected, actual);
 	}
-
+	
+	@Test
+	public void testMultFail() {
+		String actual = sut.Multiplication("5","6");
+		String expected = "56";
+		assertNotEquals(expected, actual);
+	}
+	
+	//test for wrong input format
+	@Test(expected = NumberFormatException.class)
+	public void testMult_badInput() {
+		String actual = sut.Multiplication("a", "4");
+	}
+		
+		
+		
+		
+		
+		
+	
+	@Test
+	public void testMultValSucc() {
+		String[] values = {"10", "10", "10", "10"};
+		
+		String actualAdd = sut.MultVal("add", values);
+		String actualMult = sut.MultVal("mult", values);
+		String actualSub = sut.MultVal("sub", values);
+		
+		String expectedAdd = "40";
+		String expectedMult = "10000";
+		String expectedSub = "-20";
+		
+		assertEquals(actualAdd, expectedAdd);
+		assertEquals(actualMult, expectedMult);
+		assertEquals(actualSub, expectedSub);
+	}
+	
+	@Test
+	public void testMultValFail() {
+		String[] values = {"13", "10", "10", "10"};
+		
+		String actualAdd = sut.MultVal("add", values);
+		String actualMult = sut.MultVal("mult", values);
+		String actualSub = sut.MultVal("sub", values);
+		
+		String expectedAdd = "40";
+		String expectedMult = "10000";
+		String expectedSub = "-20";
+		
+		assertNotEquals(actualAdd, expectedAdd);
+		assertNotEquals(actualMult, expectedMult);
+		assertNotEquals(actualSub, expectedSub);
+	}
+	
+	//test for wrong input format
+	@Test(expected = NumberFormatException.class)
+	public void testMultValBadInput() {
+		String[] values = {"a", "b", "c", "d"};
+		
+		String actualAdd = sut.MultVal("add", values);
+		String actualMult = sut.MultVal("mult", values);
+		String actualSub = sut.MultVal("sub", values);
+		
+	}
+	
 }
